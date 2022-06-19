99d226e7777d1faf5f8aea5feff4e6284322fcb8
Brandon Pinkerton <brandon.a.pinkerton@gmail.com>
2022-06-19 14:28:55 -0500

    rename navbar.tsx to AppNavbar.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import {SessionProvider} from "next-auth/react";
-import AppNavbar from "../components/navbar";
+import AppNavbar from "../components/AppNavbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
