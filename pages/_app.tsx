fc5aea31bb37bba28ccb0ca908e401a2e67de430
Brandon Pinkerton <brandon.a.pinkerton@gmail.com>
2022-06-19 14:17:30 -0500

    Add Google OAuth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {SessionProvider} from "next-auth/react";
+import AppNavbar from "../components/navbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <SessionProvider session={pageProps.session} refetchInterval={0}>
+          <AppNavbar />
+          <Component {...pageProps} />
+      </SessionProvider>
+  )
+
+
 }
 
 export default MyApp
